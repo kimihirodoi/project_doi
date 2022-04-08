ffb290d35af97546ccd38678ff3ee1fb3aa7d4f4
Kimihiro Doi <tookuhe35@gmail.com@gmail.com>
2022-04-08 22:36:31 +0900

    add .gitignore

--- helloworld/helloworld.cpp
+++ helloworld/helloworld.cpp
@@ -7,14 +7,15 @@ using namespace std;
 int main()
 {
    int n;
-   cin>>n;
-   vector<int>v(n,0);
-   for(int i=0;i<n+1;i++){
-      v[i]=i;
+   cin >> n;
+   vector<int> v(n, 0);
+   for (int i = 0; i < n + 1; i++)
+   {
+      v[i] = i;
    }
-   for(int i=0;i<n+1;i++){
-      cout<<v[i]<<endl;
+   for (int i = 0; i < n + 1; i++)
+   {
+      cout << v[i] << endl;
    }
-   cout<<"hhh"<<endl;
+   cout << "hhh" << endl;
 }
-
